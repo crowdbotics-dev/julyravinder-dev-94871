91b32c908fa895a8bceec57a7c72cb8d8f322738
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-06-29 15:25:40 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled6/index.js
+++ screens/Untitled6/index.js
@@ -1,7 +1,17 @@
+import { api_v1_login_create } from "../../store/julyravinderdevAPI/authTokens.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
-const Untitled6 = () => {
+const Untitled6 = ({}) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(api_v1_login_create({
+      email: "ravi@gmail.com",
+      pass: "testing"
+    }));
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={{
       backgroundColor: "#f0f0f1",
@@ -15,7 +25,7 @@ const Untitled6 = () => {
 const styles = StyleSheet.create({
   safeArea: {
     height: "100%",
-    backgroundColor: "#501010"
+    backgroundColor: "#d62020"
   }
 });
 export default Untitled6;
